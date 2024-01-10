c5a7bf2dd337dd561654d056225d3736f3444413
Nikkn123 <98714400+Nikkn123@users.noreply.github.com>
2024-01-10 12:56:45 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "19886681"))
+    API_HASH = os.environ.get("API_HASH", "600e799cf14a251134cd0c6ea8e08f27")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5142098805:AAFBUvAmCaAmMfzV7nS9zNSGxmmQmJPnLRA")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Droplinkurl_bot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
